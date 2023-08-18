6b641aeb4e2bd6aa3d9c192198da9d2d69e9848e
ElangoSelvam <elangolatha1916@gmail.com>
2023-08-18 17:38:48 +0530

    done

--- src/test/java/sample_Maven/Admin_Test.java
+++ src/test/java/sample_Maven/Admin_Test.java
@@ -4,7 +4,7 @@ import org.testng.annotations.Test;
 
 public class Admin_Test {
 	
-	@Test
+	@Test(groups = "smoke")
 	public void test() {
 		System.out.println("Test passed");
 		System.out.println("Test passed");
