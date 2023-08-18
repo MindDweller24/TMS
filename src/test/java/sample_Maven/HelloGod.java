d2e7f2b18a85a13a451ab08522010f1e83f6cf69
MindDweller24 <sanatmohapatra65@gmail.com>
2023-08-18 14:46:17 +0530

    commit

--- src/test/java/sample_Maven/HelloGod.java
+++ src/test/java/sample_Maven/HelloGod.java
@@ -2,7 +2,7 @@ package sample_Maven;
 
 public class HelloGod {
 	public static void main(String[] args) {
-		System.out.println("make me focused");
+		System.out.println("god make me focused");
 	}
 
 }
