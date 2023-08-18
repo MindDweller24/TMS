ec22860ed3470a1b7fd1fbded715a134b84d1c44
ElangoSelvam <elangolatha1916@gmail.com>
2023-08-18 15:50:12 +0530

    done

--- src/test/java/sample_Maven/UserModule.java
+++ src/test/java/sample_Maven/UserModule.java
@@ -5,8 +5,11 @@ import org.testng.annotations.Test;
 public class UserModule {
 @Test
 	public void test() {
+	System.out.println("Passed");
+	System.out.println("Passed");
 		System.out.println("Passed");
 		System.out.println("Passed");
+		//http://localhost:8080/job/Elango/
 		System.out.println("Passed");
 	}
 }
